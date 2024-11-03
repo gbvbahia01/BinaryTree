d1c55391f792b24f89c94ef1994bbfb73d4fadd3
Guilherme Braga <gbvbahia01@gmail.com>
2024-11-03 11:13:03 -0300

    distributionManagement for github

--- src/main/java/br/com/gbvbahia/collection/BinaryTreeCollection.java
+++ src/main/java/br/com/gbvbahia/collection/BinaryTreeCollection.java
@@ -7,7 +7,7 @@ import java.util.Optional;
 import java.util.Set;
 import java.util.TreeSet;
 import lombok.Getter;
- 
+
 public class BinaryTreeCollection<T extends BinaryTreeValue<T>> {
 
     @Getter
